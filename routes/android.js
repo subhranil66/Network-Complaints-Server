3089d0090fe3b745767b032d268622a202c62737
subhranil66 <subhranilsamanta450@gmail.com>
2020-01-23 14:21:49 +0530

    json code added

--- routes/android.js
+++ routes/android.js
@@ -3,7 +3,13 @@ const androidRouter=require('express').Router();
 
 androidRouter.post('/netinfo', (request, response)=>
 {
-    console.log(request.body);
+    // console.log(request.body);
+    let { address, latitude, logitude,sim_info }=request.body;
+    address=JSON.parse(address);
+    sim_info=JSON.parse(sim_info);
+    console.log(address);
+    console.log(sim_info);
+
 })
 
 
